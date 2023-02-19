bfdb61b7f8628c9429228245bff4e10dc84881e5
pulsepk <hassannazar72@gmail.com>
2023-02-20 02:50:20 +0500

    Manipulating CSS

--- Project#1 Guess My Number/script.js
+++ Project#1 Guess My Number/script.js
@@ -4,7 +4,7 @@
 document.querySelector('.message').textContent  */
 
 let random_number = Math.trunc(Math.random()*20+1);
-
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -15,12 +15,31 @@ document.querySelector('.check').addEventListener('click', function(){
     }
     else if(guess==random_number){
         document.querySelector('.message').textContent = 'You have guessed right number';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '25rem';
     }
     else if(guess>random_number){
         document.querySelector('.message').textContent = 'The guess is too high';
+        
+        if(score>1){
+        score--;
+        document.querySelector('.score').textContent = score;
+        }
+        else{
+        document.querySelector('.score').textContent = 0;   
+        }
+        
     }
     else if(guess<random_number){
         document.querySelector('.message').textContent = 'The guess is too low';
+        
+        if(score>1){
+        score--;
+        document.querySelector('.score').textContent = score;
+        }
+        else{
+        document.querySelector('.score').textContent = 0;   
+        }
     }
     
 })
