635224e63ae7b76f9f939c357a240332a7bf516a
pulsepk <hassannazar72@gmail.com>
2023-02-20 19:35:39 +0500

    Complete Project

--- Project#1 Guess My Number/script.js
+++ Project#1 Guess My Number/script.js
@@ -1,19 +1,32 @@
 'use strict';
 
-/* console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent  */
+
 
 let random_number = Math.trunc(Math.random()*20+1);
 let score = 20;
 let highscore = 0;
+
+const message = function(message){
+    document.querySelector('.message').textContent = message;
+}
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     if(!guess){
-        
-        document.querySelector('.message').textContent = 'No Value Entered';
+        message('No Value Entered');
     }
+    else if(guess!==random_number){
+        if(score>1){
+            score--;
+            document.querySelector('.score').textContent = score;
+            message(guess>random_number ? ' 📈Too High' : '📉Too Low');
+        }
+        else{
+        document.querySelector('.score').textContent = 0;   
+        } 
+        }
     else if(guess==random_number){
-        document.querySelector('.message').textContent = 'You have guessed right number';
+        
+        message('You have guessed the right number');
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '25rem';
         if(score>highscore){
@@ -28,36 +41,13 @@ document.querySelector('.check').addEventListener('click', function(){
         }
         
     }
-    else if(guess>random_number){
-        document.querySelector('.message').textContent = 'The guess is too high';
-        
-        if(score>1){
-        score--;
-        document.querySelector('.score').textContent = score;
-        }
-        else{
-        document.querySelector('.score').textContent = 0;   
-        }
-    }
-    else if(guess<random_number){
-        document.querySelector('.message').textContent = 'The guess is too low';
-        
-        if(score>1){
-        score--;
-        document.querySelector('.score').textContent = score;
-        }
-        else{
-        document.querySelector('.score').textContent = 0;   
-        }
-        
-    }
     
 })
 
 document.querySelector('.again').addEventListener('click', function(){
     let score = 20;
     random_number = Math.trunc(Math.random()*20)+1;
-    document.querySelector('.message').textContent = 'Start guessing...';
+    message('Start guessing...')
     document.querySelector('.score').textContent = score;
     document.querySelector('.guess').value = '';
     document.querySelector('body').style.backgroundColor = '#333';
